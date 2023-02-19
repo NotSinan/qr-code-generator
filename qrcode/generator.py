a0019684d0cbbebb03b3d51824b5417300de0e32
notsinan <115546292+NotSinan@users.noreply.github.com>
2023-02-20 03:26:30 +1100

    Adding new features

--- qrcode/generator.py
+++ qrcode/generator.py
@@ -1,13 +1,19 @@
 import pyqrcode
 
 
-def generate_qr_code(text: str, output_file: str = None, output_format: str = 'svg'):
+def generate_qr_code(text: str,
+                     output_file: str = None,
+                     output_format: str = 'svg',
+                     scale: int = 8,
+                     module_color: tuple = (255, 255, 255),
+                     background: str = None):
     url = pyqrcode.create(text)
+
     if output_file:
         if output_format.lower() == 'svg':
-            url.svg(output_file, scale=8)
+            url.svg(output_file, scale=scale, module_color=module_color, background=background)
         elif output_format.lower() == 'png':
-            url.png(output_file, scale=8)
+            url.png(output_file, scale=scale, module_color=module_color, background=background)
         else:
             print(f"Invalid output format: {output_format}")
             return
