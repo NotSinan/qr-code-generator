49ecd0edce93f5dbff2c1ab1ced3a1da60c30992
notsinan <115546292+NotSinan@users.noreply.github.com>
2023-02-20 17:32:35 +1100

    Added more arguments for the tool.

--- main.py
+++ main.py
@@ -3,19 +3,23 @@ from qrcode import generator
 
 
 def main():
-    # parser = argparse.ArgumentParser(description='Generate a QR code from an input string')
-    # parser.add_argument('input_string', type=str, help='Input string to generate QR code from')
-    # parser.add_argument('-o', '--output', type=str, help='Output file for generated QR code (default: qrcode.svg)', default='qrcode.svg')
-    # parser.add_argument('-f', '--format', type=str, help='Output file format (svg or png, default: svg)', default='svg')
-    # parser.add_argument('-s', '--scale', type=int, help='Size of QR code squares (default: 8)', default=8)
-    # parser.add_argument('-q', '--quiet-zone', type=int, help='Size of the quiet zone around the QR code (default: 1)',default=1)
-    # parser.add_argument('-c', '--bg-color', type=str, help='Background color of the QR code (white or black, default: white)', default='white')
-    # args = parser.parse_args()
-    #
-    # # output_file = f"{args.output_file}.{args.output_format}"
-    # generator.generate_qr_code(args.input_string, args.output, args.format, args.scale, args.quiet_zone, args.bg_color)
-    # # print(f"QR code generated successfully and saved as {output_file}!")
-    generator.generate_qr_code("Hello", "www.png", "png", 8, "#ffffff", "#FFC0CB")
+    parser = argparse.ArgumentParser(description='Generate a QR code from an input string')
+    parser.add_argument('input_string', type=str, help='Input string to generate QR code from')
+    parser.add_argument('-o', '--output', type=str, help='Output file for generated QR code (default: qrcode.svg)', default='qrcode.svg')
+    parser.add_argument('-f', '--format', type=str, help='Output file format (svg or png, default: svg)', default='svg')
+    parser.add_argument('-s', '--scale', type=int, help='Size of QR code squares (default: 8)', default=8)
+    parser.add_argument('-q', '--quiet-zone', type=int, help='Size of the quiet zone around the QR code (default: 4)',default=4)
+    parser.add_argument('-c', '--bg-color', type=str, help='Background color of the QR code', default='white')
+    parser.add_argument('-m', '--module-color', type=str, help='Module color of the QR code', default='white')
+    args = parser.parse_args()
+    output_file = f"{args.output}.{args.format}"
+    generator.generate_qr_code(text=args.input_string,
+                               output_file=output_file,
+                               output_format=args.format,
+                               scale=args.scale,
+                               module_color=args.module_color,
+                               background=args.bg_color,
+                               quiet_zone=args.quiet_zone)
 
 
 if __name__ == '__main__':
