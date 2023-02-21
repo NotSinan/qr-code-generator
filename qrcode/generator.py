401329c530eb965b8fea32b89d1e8de09fa6d893
notsinan <115546292+NotSinan@users.noreply.github.com>
2023-02-22 02:01:26 +1100

    Added error handling for files or directories that do not exist.

--- qrcode/generator.py
+++ qrcode/generator.py
@@ -8,15 +8,21 @@ def generate_qr_code(text: str,
                      module_color: str = "#FFFFFF",
                      background: str = None,
                      quiet_zone: int = 4):
+
     url = pyqrcode.create(text)
 
-    if output_file:
-        if output_format.lower() == 'svg':
-            url.svg(output_file, scale=scale, module_color=module_color, background=background, quiet_zone=quiet_zone)
-        elif output_format.lower() == 'png':
-            url.png(output_file, scale=scale, module_color=module_color, background=background, quiet_zone=quiet_zone)
+    try:
+        if output_file:
+            if output_format.lower() == 'svg':
+                url.svg(output_file, scale=scale, module_color=module_color, background=background,
+                        quiet_zone=quiet_zone)
+            elif output_format.lower() == 'png':
+                url.png(output_file, scale=scale, module_color=module_color, background=background,
+                        quiet_zone=quiet_zone)
+            else:
+                print(f"Invalid output format: {output_format}")
+                return
         else:
-            print(f"Invalid output format: {output_format}")
-            return
-    else:
-        return url.svg_as_string(scale=8)
+            return url.svg_as_string(scale=8)
+    except FileNotFoundError:
+        print(f"The file or directory does not exist: {output_file}")
