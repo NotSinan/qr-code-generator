00200d70f2957b4684eda50e6cda1e06fcc15a43
notsinan <115546292+NotSinan@users.noreply.github.com>
2023-02-21 18:37:35 +1100

    PEP8 reformatting

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from qrcode import generator
 def main():
     parser = argparse.ArgumentParser(description='Generate a QR code from an input string')
     parser.add_argument('input_string', type=str, help='Input string to generate QR code from')
-    parser.add_argument('-o', '--output', type=str, help='Output file for generated QR code (default: qrcode.svg)', default='qrcode.svg')
+    parser.add_argument('-o', '--output', type=str, help='Output file for generated QR code (default: qrcode.svg)',
+                        default='qrcode.svg')
     parser.add_argument('-f', '--format', type=str, help='Output file format (svg or png, default: svg)', default='svg')
     parser.add_argument('-s', '--scale', type=int, help='Size of QR code squares (default: 8)', default=8)
-    parser.add_argument('-q', '--quiet-zone', type=int, help='Size of the quiet zone around the QR code (default: 4)',default=4)
+    parser.add_argument('-q', '--quiet-zone', type=int, help='Size of the quiet zone around the QR code (default: 4)',
+                        default=4)
     parser.add_argument('-c', '--bg-color', type=str, help='Background color of the QR code', default='#ffffff')
-    parser.add_argument('-m', '--module-color', type=str, help='Module color of the QR code', default='#ffffff')
+    parser.add_argument('-m', '--module-color', type=str, help='Module color of the QR code', default='#111111')
     args = parser.parse_args()
     output_file = f"{args.output}.{args.format}"
     generator.generate_qr_code(text=args.input_string,
